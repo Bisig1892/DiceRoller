fef4d7c3380a456014c1af3f5e0cdccd905285e8
Tyler <bisig1892@gmail.com>
2020-07-15 22:50:19 -0700

    Created a die class

---
+++ DiceRoller/Die.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    /// <summary>
+    /// Represents an individual dice
+    /// </summary>
+    class Die
+    {
+        /// <summary>
+        /// Face value of die
+        /// </summary>
+        public byte Value { get; set; }
+
+        public bool IsHeld { get; set; }
+
+        // Roll method
+    }
+}
