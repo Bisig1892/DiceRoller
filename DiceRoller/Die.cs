4033592e7eb4e94e89e28215322afede9442f374
Tyler <bisig1892@gmail.com>
2020-07-15 23:22:18 -0700

    roll method with random number generator

--- DiceRoller/Die.cs
+++ DiceRoller/Die.cs
@@ -11,13 +11,38 @@ namespace DiceRoller
     /// </summary>
     class Die
     {
+        // Static fields are shared across all instances of a class
+        static Random rand;
+
+        // Static constructors are called once for all instances of the class
+        static Die()
+        {
+            rand = new Random();
+        }
+
+        public Die()
+        {
+            Roll(); // Roll die on creation to generate first random number
+        }
+
         /// <summary>
         /// Face value of die
         /// </summary>
-        public byte Value { get; set; }
+        public byte Value { get; private set; }
 
         public bool IsHeld { get; set; }
 
-        // Roll method
+        /// <summary>
+        /// Roll a new random value between 1-6 and return the newly rolled value
+        /// If the die is held, the current value will be returned and no new value generated
+        /// </summary>
+        public byte Roll()
+        {
+            if (!IsHeld)
+            {
+                Value = (byte)rand.Next(1, 7);
+            }
+            return Value;
+        }
     }
 }
